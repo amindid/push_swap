f34727b444e3719dfe351f8c6340b5d62af91302
Amine Ouchaadid <aouchaad@c2r1p5.1337.ma>
2023-03-17 17:53:22 +0100

    new uploads

---
+++ free_stacks.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_stacks.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouchaad <aouchaad@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 19:04:34 by aouchaad          #+#    #+#             */
+/*   Updated: 2023/03/15 19:05:18 by aouchaad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	free_stacks(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*temp;
+
+	while (*stack_a)
+	{
+		temp = (*stack_a)->next;
+		free(*stack_a);
+		*stack_a = temp;
+	}
+	while (*stack_b)
+	{
+		temp = (*stack_b)->next;
+		free(*stack_b);
+		*stack_b = temp;
+	}
+}
